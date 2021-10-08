1ef810eea7ac88381605e8e4c2f2c48253973345
RachelRebecca <rachelrebecca67@gmail.com>
2021-10-08 11:31:27 -0400

    removed comments

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -49,31 +49,21 @@ public class EchoClient
         try
                 (
                         Socket echoSocket = new Socket(hostName, portNumber);
-                        //requires host's IP address (hostname), has to be the same port number as servers
-                        //this becomes the client socket that is getting accepted by the server
                         PrintWriter out =
                                 new PrintWriter(echoSocket.getOutputStream(), true);
                         BufferedReader in =
                                 new BufferedReader(
                                         new InputStreamReader(echoSocket.getInputStream()));
-                        // when a message comes from server into echo socket, read that input stream (a.k.a. message)
                         BufferedReader stdIn =
                                 new BufferedReader(
                                         new InputStreamReader(System.in))
-                        // standard input to read from the user,
-                        // cuz we want the user to type in some commands which the client will then send it to server
                 )
         {
             String userInput;
             while ((userInput = stdIn.readLine()) != null)
-            // what user types is assigned to standard input using output stream
-            // what you get back from server is displayed to the stream (it's our very message)
             {
                 out.println(userInput);
-                // which is the server's input which really the output from the client which is really the user's input
-                // this is when it's sent to the server
-                System.out.println("echo: " + in.readLine()); //read the next line coming in from the server
-                //THE RESPONSE: output from server, input from client, out to the user
+                System.out.println("echo: " + in.readLine());
             }
         }
         catch (UnknownHostException e)
--- src/EchoServer.java
+++ src/EchoServer.java
@@ -34,7 +34,7 @@ import java.io.*;
 
 public class EchoServer
 {
-    public static void main(String[] args) throws IOException //excepts arguments
+    public static void main(String[] args) throws IOException
     {
 
         if (args.length != 1)
@@ -43,49 +43,21 @@ public class EchoServer
             System.exit(1);
         }
 
-        //should have some sort of validation
         int portNumber = Integer.parseInt(args[0]);
 
         try
-            // certain resources are taken from the OS and object is able to use them.
-            // But object needs to be told when it's no longer in use.
-            // Garbage collector causes memory to be free when port's no longer in use,
-            // but what tells OS that port is now available for use?
-            // This process is called disposal.
-            // This try with resources block: when try is done,
-            // the dispose method is called automatically.
-            // access 2 streams (out and in) - can send a message to another computer
-            // and receive a message from another computer
                 (
                         ServerSocket serverSocket =
                                 new ServerSocket(Integer.parseInt(args[0]));
-                        // like the host, not looking for someone specific, but wait for someone to send to it.
-                        // The port is the application, every app has a port
-                        // Server is like a parking lot - each person in apartment has one spot -
-                        // the client is the one who has the car and drives to address and parks in his spot,
-                        // and he must park in his own spot - whenever trying to establish the connection it's at a port.
-                        // The server socket has to say, I'm accepting messages coming at this particular port (args[0])
-                        // but any computer can send messages to this port.
-                        // Creating a server socket creates a sort of plug in. When something plugs in, a client has plugged
-                        // - no specifically which client is plugging in ("plugging in" happens via the internet)
-
                         Socket clientSocket = serverSocket.accept();
-                        // accept method accepts what's coming to the server socket and returns a socket
-                        // A client computer sends something to this socket. We're not specifying the computer
-                        // As long as you are actually something, we'll call this the client.
                         PrintWriter out =
                                 new PrintWriter(clientSocket.getOutputStream(), true);
                         BufferedReader in = new BufferedReader(
                                 new InputStreamReader(clientSocket.getInputStream()));
-                        // out sends something to output stream to the client socket
-                        // in reads from the client
                 )
-        //everything in parenthesis is treated like a resource,
-        // so we don't have to manually dispose all these things to free them when we're done
         {
             String inputLine;
-            while ((inputLine = in.readLine()) != null)//use the in stream
-            // as long as you receive that message, spit it back out to the other computer.
+            while ((inputLine = in.readLine()) != null)
             {
                 out.println(inputLine);
             }
